1bd6a8c248c5d0052146391a31f7ecd716dfff37
Luis Dario Rodríguez <luisdario500@gmail.com>
2018-11-14 19:05:55 -0600

    Subir archivo GenisysATM a github

---
+++ GenisysATM/GenisysATM/frmMenuPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Aplicando MaterialSkin
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace GenisysATM
+{
+    public partial class frmMenuPrincipal : MaterialForm
+    {
+        private readonly MaterialSkinManager materialSkinManager;
+
+        public frmMenuPrincipal()
+        {
+            InitializeComponent();
+
+            // Implementar temas y colores
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue500, Primary.Blue700, Primary.Blue200,
+                Accent.Orange400, TextShade.WHITE
+            );
+        }
+    }
+}
